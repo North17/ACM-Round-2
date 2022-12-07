218643a290a05b86d5162609d1d3ac24405ce235
Marc Stanley <marc.stan17@gmail.com>
2022-12-07 21:31:52 +0530

    Added database model for posts

--- Backend/Django/django_project/blog/views.py
+++ Backend/Django/django_project/blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'MarcS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2022'
-    },
-    {
-        'author': 'JaneDoe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2022'
-    }
-]
+# posts = [
+#     {
+#         'author': 'MarcS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2022'
+#     },
+#     {
+#         'author': 'JaneDoe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2022'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
